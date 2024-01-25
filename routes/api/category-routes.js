a49de9bb2bec9410d58cc5ff57f61ec04cdb6b71
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-25 00:28:28 -0500

    finished the category routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+
 
 router.get('/', async (req, res) => {
+  const allCategories = await Category.findAll();
+  const allProducts = await Category.findAll();
+
+  return res.json(allCategories);
+  return res.json(allProducts);
   // find all categories
   // be sure to include its associated Products
 });
@@ -14,15 +19,28 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+  const allCategories = await Category.create(req.body);
+
+  return res.json(allCategories);
   // create a new category
 });
 
 router.put('/:id', async (req, res) => {
+  const allCategories = await Category.findByPk(req.params.id);
+
+  return res.json(allCategories);
   // update a category by its `id` value
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const allCategories = await Category.destroy({
+    where: {
+      category_id: req.params.category_id,
+    },
+  });
+
+  return res.json(allCategories);
 });
 
 module.exports = router;
