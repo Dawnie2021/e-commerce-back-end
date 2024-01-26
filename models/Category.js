69c0da5e97d4622d5f0640e3737bde4d5597dd34
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-25 23:40:33 -0500

    added id values in catergory.js

--- models/Category.js
+++ models/Category.js
@@ -6,10 +6,18 @@ class Category extends Model { }
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     category_name: {
-      type: DataTypes.STRING
-    }
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
+
   {
     sequelize,
     timestamps: false,
