0769c0e06cc6db9659dc866f95ad66a0dcee3b54
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-26 00:27:21 -0500

    executed association methods on models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+
+});
+
+Product.belongsToMany(Tag, { through: ProductTag });
+
+Tag.belongsToMany(Product, { through: ProductTag });
+
+// DO I HAVE TO ADD A KEY TO BELONGS TO MANY? 
 
 module.exports = {
   Product,
