3ec2445bd2567b28cab32f24bd60b1559fb7eb83
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-25 23:47:56 -0500

    added values in tag.js

--- models/Product.js
+++ models/Product.js
@@ -2,35 +2,35 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class Product extends Model {}
+class Product extends Model { }
 
 
 Product.init(
   {
-   product_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-    // validate that the value is a decimal
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate that the value is a decimal
 
-  },
-  stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    // Set a default value of 10
-    // Validates that the value is numeric
-  },
-  category_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Set a default value of 10
+      // Validates that the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true
       // References the category model's id
-  }
-},
+    }
+  },
   {
     sequelize,
     timestamps: false,
