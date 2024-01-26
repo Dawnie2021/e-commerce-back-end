79bfd3f0c9a12ce45e0dc7c3bef0bb48417f5d2e
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-25 23:44:44 -0500

    added values in product.js

--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,27 @@ class Product extends Model {}
 Product.init(
   {
    product_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
   },
   price: {
-    type: DataTypes.INTEGER
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    // validate that the value is a decimal
+
   },
   stock: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    // Set a default value of 10
+    // Validates that the value is numeric
   },
   category_id: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+      // References the category model's id
   }
 },
   {
